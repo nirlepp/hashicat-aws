3bce58f3488fe153ad2340ddcd9c620c0703d003
Nirlep Patel <patel.nirlep@gmail.com>
2020-11-17 20:36:29 +0000

    updated bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket1231231"
   acl    = "private"
-  bucket_prefix = "nirleppatel"
 
   versioning = {
     enabled = true
